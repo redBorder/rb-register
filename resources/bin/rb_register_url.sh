a0f8090b9e1082f66738b33809fce7e57422d5f2
Miguel <manegron@redborder.com>
2022-03-25 11:19:56 +0000

    Add services in etc/hosts

--- resources/bin/rb_register_url.sh
+++ resources/bin/rb_register_url.sh
@@ -98,6 +98,8 @@ fi
 sed -i '/data.redborder.cluster/d' /etc/hosts
 sed -i '/rbookshelf.s3.redborder.cluster/d' /etc/hosts
 [ "x$RBDOMAINIP" != "x" ] && echo "$RBDOMAINIP data.redborder.cluster rbookshelf.s3.redborder.cluster redborder.cluster s3.service erchef.service" >> /etc/hosts
+
+sed -i '/kafka.service/d' /etc/hosts
 echo "127.0.0.1 kafka.service zookeeper.service f2k.service logstash.service freeradius.service n2klocd.service rb-ale.service rb-nmsp.service rsyslog.service" >> /etc/hosts
 #fi
 
