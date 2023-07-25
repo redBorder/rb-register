446c715404af8bcd965eb70d0940363ae8b97785
vimesa <vimesa@redborder.com>
2023-07-25 08:45:11 +0100

    Modify kafka.service in /etc/hosts

--- resources/bin/rb_register_url.sh
+++ resources/bin/rb_register_url.sh
@@ -119,6 +119,11 @@ sed -i '/kafka.service/d' /etc/hosts
 echo "127.0.0.1 kafka.service zookeeper.service f2k.service logstash.service freeradius.service n2klocd.service rb-ale.service rb-nmsp.service rsyslog.service" >> /etc/hosts
 #fi
 
+if [ $TYPE -eq "ips" ]
+  sed -i '/kafka.service/d' /etc/hosts
+  [ "x$RBDOMAINIP" != "x" ] && echo "$RBDOMAINIP data.redborder.cluster rbookshelf.s3.redborder.cluster redborder.cluster s3.service erchef.service http2k.service webui.service kafka.service" >> /etc/hosts
+fi
+
 if [ $START -eq 1 ]; then
   if [ -f /etc/chef/client.pem ]; then
     systemctl status chef-client &>/dev/null
