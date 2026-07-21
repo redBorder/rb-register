1e601eab71d9053a71aa49e96bedbea77b98698d
Nils <nverschaeve@redborder.com>
2026-07-21 11:57:30 +0100

    remove obsolete rb_configure_cgroups call from registration finish script

--- resources/bin/rb_register_finish.sh
+++ resources/bin/rb_register_finish.sh
@@ -97,9 +97,6 @@ if [ -f /etc/chef/role-once.json.default ]; then
     sleep 5
     rb_wakeup_chef.sh
   fi
-  
-  title "Configuring cgroups (first time) please wait..."
-  rb_configure_cgroups &>/dev/null 
  
   end_script=$(date +%s) # Save finish scrip time
   runtime=$((end_script-start_script)) # Calculate duration of script
